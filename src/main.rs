f1812b874b7b1257f98e38c1a68c4fc6cbc2ee28
Paul DE TEMMERMAN <xf4@3ds.com>
2022-08-09 16:27:10 +0200

    stats on entire puzzle csv file

--- src/main.rs
+++ src/main.rs
@@ -5,64 +5,111 @@ extern crate serde;
 
 use std::error::Error;
 use std::io;
+use std::env;
 use serde::Deserialize;
 
 
-#[derive(Deserialize)]
-struct RatingAttempts {
+#[derive(Deserialize, Default, Clone)]
+struct PuzzleEntry {
+    id: String,
+    _fen: String,
+    _moves: String,
     rating: u64,
-    attemps: u64,
+    rating_deviation: u64,
+    popularity: i64,
+    attempts: u64,
+    _themes: String,
+    _url: String,
+    _opening_family: String,
+    _opening_variation: String,
+}
+
+#[derive(Clone, Default)]
+struct Bucket {
+    count: u64,
+    rating: u64,
+    rating_deviation: u64,
+    attempts: u64,
 }
 
 fn main() {
-    let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_reader(io::stdin());
+
+    // let mut rdr = csv::ReaderBuilder::new().has_headers(false).flexible(true).from_reader(io::stdin());
     let mut count = 0;
     let mut total_ratings : u64 = 0;
-    let mut total_attemps : u64 = 0;
-    let mut buckets_attempts = Vec::<u64>::new();
-    let mut buckets_count = Vec::<u64>::new();
+    let mut total_attempts : u64 = 0;
+    let mut buckets = Vec::<Bucket>::new();
     let bucket_range = 100;
+    let mut highest_rated: PuzzleEntry = PuzzleEntry::default();
+    let mut lowest_rated: PuzzleEntry = PuzzleEntry::default();
+    lowest_rated.rating = u64::MAX;
+    let mut least_popular: PuzzleEntry = PuzzleEntry::default();
+    least_popular.popularity = i64::MAX;
+    let mut most_played: PuzzleEntry = PuzzleEntry::default();
+    let mut least_played: PuzzleEntry = PuzzleEntry::default();
+    least_played.attempts = u64::MAX;
 
-    for line in rdr.records() {
-        let record: RatingAttempts = line.unwrap().deserialize(None).unwrap(); 
+    //for line in rdr.records() {
+        //let line = line.unwrap();
+        // let record: PuzzleEntry = line.deserialize(None).unwrap();
+    for line in std::io::stdin().lines() {
+        let line = line.unwrap();
+        let chunks : Vec::<&str> = line.split(",").collect();
+        let mut record = PuzzleEntry::default();
+        record.id = if chunks.len() > 0 { chunks[0].to_string() } else { String::new() };
+        record._fen = if chunks.len() > 1 { chunks[1].to_string() } else { String::new() };
+        record._moves = if chunks.len() > 2 { chunks[2].to_string() } else { String::new() };
+        record.rating = if chunks.len() > 3 { chunks[3].parse::<u64>().unwrap_or(0).try_into().unwrap_or(0) } else { 0 };
+        record.rating_deviation = if chunks.len() > 4 { chunks[4].parse::<u64>().unwrap_or(0).try_into().unwrap_or(0) } else { 0 };
+        record.popularity = if chunks.len() > 5 { chunks[5].parse::<i64>().unwrap_or(0).try_into().unwrap_or(0) } else { 0 };
+        record.attempts = if chunks.len() > 6 { chunks[6].parse::<i64>().unwrap_or(0).try_into().unwrap_or(0) } else { 0 };
+        record._themes = if chunks.len() > 7 { chunks[7].to_string() } else { String::new() };
+        record._url = if chunks.len() > 8 { chunks[8].to_string() } else { String::new() };
+        record._opening_family = if chunks.len() > 9 { chunks[9].to_string() } else { String::new() };
+        record._opening_variation = if chunks.len() > 10 { chunks[10].to_string() } else { String::new() };
+
         total_ratings += record.rating;
-        total_attemps += record.attemps;
+        total_attempts += record.attempts;
 
         let bucket_index : usize = (record.rating / bucket_range) as usize;
-        if buckets_attempts.len() <= bucket_index {
-            buckets_attempts.resize(bucket_index + 1, 0);
-            buckets_count.resize(bucket_index + 1, 0);
+        if buckets.len() <= bucket_index {
+            buckets.resize(bucket_index + 1, Bucket::default());
         }
 
-        buckets_attempts[bucket_index] += record.attemps;
-        buckets_count[bucket_index] += 1;
+        let b: &mut Bucket = &mut buckets[bucket_index];
+        b.count += 1;
+        b.rating += record.rating;
+        b.rating_deviation += record.rating_deviation;
+        b.attempts += record.attempts;
 
-        if record.rating >= 2900 {
-            println!("{} {} {}", count, record.rating, record.attemps);
+        if highest_rated.rating < record.rating { highest_rated = record.clone(); }
+        if lowest_rated.rating > record.rating { lowest_rated = record.clone(); }
+        if least_popular.popularity > record.popularity { least_popular = record.clone(); }
+        if most_played.attempts < record.attempts { most_played = record.clone(); }
+        if least_played.attempts > record.attempts { least_played = record.clone(); }
+
+        if record.rating >= 2900 || record.rating < 550 {
+            println!("[{}] {} / rating: {} / attempts: {} / deviation: {}", count, record.id, record.rating, record.attempts, record.rating_deviation);
         }
 
-        if count < 10 {
-            //println!("{} {} {}", count, record.rating, record.attemps);
-        }
         count += 1;
-
-        if count > 1000 {
-            //break;
-        }
-
     }
 
     println!("Rating: {} ({} avg)", total_ratings, (total_ratings as f64) / count as f64);
-    println!("Attempts: {} ({} avg)", total_attemps, (total_attemps as f64) / count as f64);
+    println!("Attempts: {} ({} avg)", total_attempts, (total_attempts as f64) / count as f64);
+    println!("Highest rated: {} {} {}", highest_rated.id, highest_rated.rating, highest_rated.attempts);
+    println!("Lowest rated: {} {} {}", lowest_rated.id, lowest_rated.rating, lowest_rated.attempts);
+    println!("Most played: {} {} {}", most_played.id, most_played.rating, most_played.attempts);
+    println!("Least played: {} {} {}", least_played.id, least_played.rating, least_played.attempts);
 
     println!("Buckets");
     let mut bi = 0;
     let mut all_zeros = true;
-    for b in buckets_attempts {
-        let bc = buckets_count[bi];
-        if !(all_zeros && bc == 0) {
+    println!("{}, {}, {}, {}, {}, {}", "rating", "count", "attempts", "deviation", "attempts/puzzle", "deviation/puzzle");
+    for b in buckets {
+        if !(all_zeros && b.count == 0) {
             all_zeros = false;
-            println!("{}, {}, {}", bi as u64 * bucket_range, bc, b);
+            println!("{}, {}, {}, {}, {}, {}", bi as u64 * bucket_range, b.count, b.attempts, b.rating_deviation, b.attempts as f64 / b.count as f64, b.rating_deviation as f64 / b.count as f64);
         }
         bi += 1;
     }
